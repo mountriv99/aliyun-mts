63c4933055a3dbcb237886d2350f60d194131f41
Eric <mountriv99@yahoo.ca>
2019-03-14 19:06:51 +0800

    added sample params to add_media_workflow

--- lib/aliyun/mts/media_workflow.rb
+++ lib/aliyun/mts/media_workflow.rb
@@ -4,36 +4,80 @@ module Aliyun
 
 			class << self
 
-				# https://help.aliyun.com/document_detail/29226.html
+				# https://help.aliyun.com/document_detail/44437.html
+				# https://help.aliyun.com/document_detail/68617.html
 				# params: {
-				#   :Input => {"Bucket":"example-bucket", "Location":"oss-cn-hangzhou", "Object":"example.flv"},
-				#   :OuputBucket => "example-output-bucket",
-				#   :OutputLocation => "oss-cn-hangzhou", # optional, defaults to "oss-cn-hangzhou"
-				#   :Outputs => [{
-				#      "OutputObject":"example-output.flv",
-				#      "TemplateId":"S00000000-000010", # https://help.aliyun.com/document_detail/29256.html
-				#      "WaterMarks":[{"InputFile":{"Bucket":"example-bucket","Location":"oss-cn-hangzhou","Object":"example-logo.png"},"WaterMarkTemplateId":"88c6ca184c0e47098a5b665e2a126797"}],
-				#      "UserData":"testid-001"
-				#   }],
-				#   :PipelineId => "example-pipeline-id"
+				#   "Topology": {
+				#     "Activities": {
+				#       "Act-Transcode-M3U8": {
+				#         "Parameters": {
+				#           "Outputs": [{
+				#             "OutputObject": "transcode%2F%7BObjectPrefix%7D%7BFileName%7D",
+				#             "TemplateId": "957d1719ee85ed6527b90cf62726cbef"
+				#           }],
+				#           "OutputBucket": "panda-vod-hls",
+				#           "OutputLocation": "oss-cn-hangzhou"
+				#         },
+				#         "Type": "Transcode"
+				#       },
+				#       "Act-Start": {
+				#         "Name": "Act-Start",
+				#         "Parameters": {
+				#           "PipelineId": "130266f58161436a80bf07cb12c8009a",
+				#           "InputFile": {
+				#             "Bucket": "panda-vod",
+				#             "Location": "oss-cn-hangzhou"
+				#           }
+				#         },
+				#         "Type": "Start"
+				#       },
+				#       "Act-Report": {
+				#         "Name": "Act-Report",
+				#         "Parameters": {},
+				#         "Type": "Report"
+				#       }
+				#     },
+				#     "Dependencies": {
+				#       "Act-Transcode-M3U8": ["Act-Report"],
+				#       "Act-Start": ["Act-Transcode-M3U8"],
+				#       "Act-Report": []
+				#     }
+				#   },
+				#   "TriggerMode": "" # optional: "OssAutoTrigger" or "NotInAuto"
 				# }
-				def submit_jobs(params)
-					params = params.merge({:Action => "SubmitJobs"})
-					params[:Input] = params[:Input].to_json
-					params[:Outputs] = params[:Outputs].to_json
+				def add_media_workflow(params)
+					params = params.merge({:Action => "AddMediaWorkflow"})
+					params[:Name] ||= "mediaworkflow-#{Time.now.to_f}-#{SecureRandom.hex(10)}"
+					params[:Topology] = params[:Topology].to_json
 
 					Util.get_response(params)
 				end
 
-				def cancel_job
+				def activate_media_workflow
 					# TODO
 				end
 
-				def query_job_list
+				def deactivate_media_workflow
 					# TODO
 				end
 
-				def list_job
+				def delete_media_workflow
+					# TODO
+				end
+
+				def update_media_workflow
+					# TODO
+				end
+
+				def query_media_workflow_list
+					# TODO
+				end
+
+				def search_media_workflow
+					# TODO
+				end
+
+				def update_media_workflow_trigger_mode
 					# TODO
 				end
 
