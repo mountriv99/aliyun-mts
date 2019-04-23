6e842c1caa19e7ff92438262c46539d36bc7da79
Eric <mountriv99@yahoo.ca>
2019-04-23 19:37:51 +0800

    removed vod

--- lib/aliyun/mts.rb
+++ lib/aliyun/mts.rb
@@ -6,6 +6,8 @@ Dir[File.join(__dir__, 'mts', '*.rb')].each { |file| require_relative file }
 module Aliyun
 	module MTS
 		class << self
+			Version = "2014-06-18"
+
 			attr_accessor :access_key_id, :access_key_secret, :region, :debug
 
 			def base_url
